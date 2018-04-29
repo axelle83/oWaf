ad435711a3140b035e23d10ed58c888ff33a587e
chrissou <nicolchristelle@gmail.com>
2018-04-29 16:30:10 +0200

    integration home visiteur responsive

--- React/app/src/components/HomeVisiteur/index.js
+++ React/app/src/components/HomeVisiteur/index.js
@@ -16,7 +16,22 @@ import React from 'react';
  */
 const HomeVisiteur = () => (
   <div id="homeVisiteur">
-    Home Visiteur
+
+    <div className="visiteur-img" />
+    <div className="visiteur-text-img">Vous êtes propriétaire de chien et vous voulez partager des lieux dogfriendly</div>
+
+    <div className="visiteur-carte">
+      <div className="visiteur-carte-img" />
+      <div className="visiteur-carte-text">Venez partager vos lieux de détente favoris et découvrir ceux des autres ! </div>
+    </div>
+
+    <div className="visiteur-img2">
+      <div className="visiteur-img2-text">Vous partez en déplacement et vous ne savez pas ou détendre votre chien ? Restez zen la communauté oWaf est là pour vous...</div>
+      <div className="visiteur-img2-img" />
+    </div>
+
+    <div className="visiteur-articles">Article à la une</div>
+
   </div>
 );
 
