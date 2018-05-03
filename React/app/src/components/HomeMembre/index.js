d8d7c2e1c2ed78d89343ad2deca31714c72056d2
chrissou <nicolchristelle@gmail.com>
2018-05-03 22:58:26 +0200

    debut pager membre et position btn chat et contact

--- React/app/src/components/HomeMembre/index.js
+++ React/app/src/components/HomeMembre/index.js
@@ -16,7 +16,21 @@ import React from 'react';
  */
 const HomeMembre = () => (
   <div id="homeMembre">
-    Home Membre
+
+
+    <div className="homeMembre-img" />
+    <div className="homeMembre-text">Bienvenue JeanMachin et une caresse à Médore </div>
+
+
+    <div className="homeMembre-carte-title">Carte intéractive</div>
+    <div className="homeMembre-carte">carte</div>
+    <div className="homeMembre-carte-texte">Venez partagez vos lieux de détente favoris et découvrir ceux des autres</div>
+
+
+    <div className="homeMembre-blog">Discussions à la une</div>
+    <div className="homeMembre-blog-article">articles</div>
+
+
   </div>
 );
 
