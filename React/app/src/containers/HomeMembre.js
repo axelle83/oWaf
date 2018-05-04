3cb848c0773686719d03237b3815e86049976789
chrissou <nicolchristelle@gmail.com>
2018-05-04 12:38:22 +0200

    bouton fixe, chat fixe

---
+++ React/app/src/containers/HomeMembre.js
@@ -0,0 +1,26 @@
+/**
+ * Npm import
+ */
+import { connect } from 'react-redux';
+// import { bindActionCreators } from 'redux';
+
+/**
+ * Local import
+ */
+import HomeMembre from 'src/components/HomeMembre';
+// import datas from 'src/datas/subscribe';
+
+/*
+ * Code
+ */
+// State
+const mapStateToProps = () => null;
+
+// Actions
+const mapDispatchToProps = () => {};
+
+
+/*
+ * Export default
+ */
+export default connect(mapStateToProps, mapDispatchToProps)(HomeMembre);
