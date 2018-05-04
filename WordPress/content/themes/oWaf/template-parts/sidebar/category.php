d76cdb8ded2e2b3bb94dcffba1a2beaf6575601d
angeliquepnn <penon.angelique@gmail.com>
2018-05-04 12:58:00 +0200

    test

--- WordPress/content/themes/oWaf/template-parts/sidebar/category.php
+++ WordPress/content/themes/oWaf/template-parts/sidebar/category.php
@@ -1,6 +1,3 @@
 <ul>
-<li><?php the_category(); ?></li>
-<!-- <li><a href="#">Alimentation</a></li>
-<li><a href="#">Soins</a></li>
-<li><a href="#">Trucs et astuces</a></li> -->
+  <li><?php the_category(); ?></li>
 </ul>
