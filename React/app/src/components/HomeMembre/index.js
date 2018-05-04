afa38b3e8d3efebb0821abf57a1f252911c0e198
chrissou <nicolchristelle@gmail.com>
2018-05-04 10:28:18 +0200

    page membre integration, page visiteur correction de bug, btn chat et contact version tablette

--- React/app/src/components/HomeMembre/index.js
+++ React/app/src/components/HomeMembre/index.js
@@ -21,11 +21,11 @@ const HomeMembre = () => (
     <div className="homeMembre-img" />
     <div className="homeMembre-text">Bienvenue JeanMachin et une caresse à Médore </div>
 
-
     <div className="homeMembre-carte-title">Carte intéractive</div>
-    <div className="homeMembre-carte">carte</div>
-    <div className="homeMembre-carte-texte">Venez partagez vos lieux de détente favoris et découvrir ceux des autres</div>
-
+    <div className="homeMembre-carte">
+      <div className="homeMembre-carte-img" />
+      <div className="homeMembre-carte-texte">Venez partagez vos lieux de détente favoris et découvrir ceux des autres !</div>
+    </div>
 
     <div className="homeMembre-blog">Discussions à la une</div>
     <div className="homeMembre-blog-article">articles</div>
