3dc5c015d6d40d45a8d11f80deedac79ddf58fe7
angeliquepnn <penon.angelique@gmail.com>
2018-05-07 14:29:25 +0200

    image header done

--- WordPress/content/themes/oWaf/inc/theme-support.php
+++ WordPress/content/themes/oWaf/inc/theme-support.php
@@ -11,8 +11,8 @@ function owaf_setup() {
   add_theme_support('title-tag');
   //Pour le custom header - image -
   $args = array(
-    'width'         => 980,
-    'height'        => 550,
+    'width'         => 1024,
+    'height'        => 650,
     'default-image' => get_template_directory_uri() . '/app/assets/images/header.jpg',
     'uploads'       => true,
 );
