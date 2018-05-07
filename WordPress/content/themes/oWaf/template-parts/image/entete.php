606fd09de31b761ed180d2e1d2accf1ea21572f9
angeliquepnn <penon.angelique@gmail.com>
2018-05-07 09:56:31 +0200

    debug header ok

--- WordPress/content/themes/oWaf/template-parts/image/entete.php
+++ WordPress/content/themes/oWaf/template-parts/image/entete.php
@@ -1,3 +1,6 @@
+<img  class="header__image" style="background-image: url(<?php header_image();  ?>);" >
 
 
-<img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="" />
+
+
+<!-- <img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="" /> -->
