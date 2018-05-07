5dd09039173ae2dc44b85147214dc6c1b4b16424
ingrid labeaume <cactushappy27@gmail.com>
2018-05-07 10:26:10 +0200

    custom_header_test

--- WordPress/content/themes/oWaf/inc/theme-support.php
+++ WordPress/content/themes/oWaf/inc/theme-support.php
@@ -13,7 +13,7 @@ function owaf_setup() {
   $args = array(
 	'width'         => 980,
 	'height'        => 200,
-	'default-image' => get_template_directory_uri() . '/images/header.jpg',
+	'default-image' => get_template_directory_uri() . '/app/assets/images/header.jpg',
 	'uploads'       => true,
 );
 add_theme_support( 'custom-header', $args );
