10150f46a9e01d395173790f72b465bdf542da28
ingrid labeaume <cactushappy27@gmail.com>
2018-05-07 10:04:08 +0200

    in english please

--- WordPress/content/plugins/owaf-settings/inc/functions.php
+++ WordPress/content/plugins/owaf-settings/inc/functions.php
@@ -39,17 +39,17 @@ wp_add_dashboard_widget('example_dashboard_widget', 'Mon Widget Personnalisé', 
 add_action('wp_dashboard_setup', 'example_add_dashboard_widgets' );
 
 
-
 //
-function themename_custom_header_setup() {
-    $args = array(
-        'default-image'      => get_template_directory_uri() . 'img/default-image.jpg',
-        'default-text-color' => '000',
-        'width'              => 1000,
-        'height'             => 250,
-        'flex-width'         => true,
-        'flex-height'        => true,
-    )
-    add_theme_support( 'custom-header', $args );
-}
-add_action( 'after_setup_theme', 'themename_custom_header_setup' );
+// //customiser l'image header
+// function themename_custom_header_setup() {
+//     $args = array(
+//         'default-image'      => get_template_directory_uri() . 'img/default-image.jpg',
+//         'default-text-color' => '000',
+//         'width'              => 1000,
+//         'height'             => 250,
+//         'flex-width'         => true,
+//         'flex-height'        => true,
+//     );
+//     add_theme_support( 'custom-header', $args );
+// }
+// add_action( 'after_setup_theme', 'themename_custom_header_setup' );
