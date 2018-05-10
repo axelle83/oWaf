1fd48c549ddbdf7c7268b1b29fba6383f5e4572c
angeliquepnn <penon.angelique@gmail.com>
2018-05-10 13:57:04 +0200

    modif erreur fatale

--- WordPress/content/themes/oWaf/inc/theme-support.php
+++ WordPress/content/themes/oWaf/inc/theme-support.php
@@ -58,21 +58,21 @@ add_action('after_setup_theme', 'owaf_setup');
 
 
 // Map google_api_key
-function my_theme_add_scripts() {
-    if (is_page('map')) {
-        wp_enqueue_script( 'google-map', 'https://maps.googleapis.com/maps/api/js?key=AIzaSyA2BwnfF3T6Nd_filKP5OIT7wfqsnsDyDo', array(), '3', true );
-        wp_enqueue_script( 'google-map-init', get_template_directory_uri() . '/js/google-maps.js', array('google-map', 'jquery'), '0.1', true );
-    }
-}
-
-add_action( 'wp_enqueue_scripts', 'my_theme_add_scripts' );
-
-function my_acf_google_map_api( $api ){
-
-	$api['key'] = 'AIzaSyA2BwnfF3T6Nd_filKP5OIT7wfqsnsDyDo';
-
-	return $api;
-
-}
-
-add_filter('acf/fields/google_map/api', 'my_acf_google_map_api');
+// function my_theme_add_scripts() {
+//     if (is_page('map')) {
+//         wp_enqueue_script( 'google-map', 'https://maps.googleapis.com/maps/api/js?key=AIzaSyA2BwnfF3T6Nd_filKP5OIT7wfqsnsDyDo', array(), '3', true );
+//         wp_enqueue_script( 'google-map-init', get_template_directory_uri() . '/js/google-maps.js', array('google-map', 'jquery'), '0.1', true );
+//     }
+// }
+//
+// add_action( 'wp_enqueue_scripts', 'my_theme_add_scripts' );
+//
+// // function my_acf_google_map_api( $api ){
+// //
+// // 	$api['key'] = 'AIzaSyA2BwnfF3T6Nd_filKP5OIT7wfqsnsDyDo';
+// //
+// // 	return $api;
+// //
+// // }
+//
+// add_filter('acf/fields/google_map/api', 'my_acf_google_map_api');
