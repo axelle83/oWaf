8b2de8157b62de6fc428201b3f5262003014c7c9
ingrid labeaume <cactushappy27@gmail.com>
2018-05-11 14:36:22 +0200

    correctif restapi

--- WordPress/content/plugins/owaf-settings/inc/settings_rest_api.php
+++ WordPress/content/plugins/owaf-settings/inc/settings_rest_api.php
@@ -6,168 +6,19 @@
    Author: oWaf
    */
 
-add_action( 'rest_api_init', 'owaf_register_rest_fields' );
+// Require des fichiers de mise en place de l'api
+require plugin_dir_path(__FILE__) . 'rest_api/lieu.php';
+require plugin_dir_path(__FILE__) . 'rest_api/dog.php';
+require plugin_dir_path(__FILE__) . 'rest_api/user.php';
 
-function owaf_register_rest_fields() {
+// Ajout au hook rest_api_init des mes modifications de la rest api
+add_action( 'rest_api_init', 'owaf_lieu_register_rest_fields' );
+add_action( 'rest_api_init', 'owaf_dog_register_rest_fields' );
+add_action( 'rest_api_init', 'owaf_user_register_rest_fields' );
 
-  register_rest_field(
-    'lieu',
-    'adresse',
-    [
-      'get_callback' => 'owaf_rest_api_adresse',
-      'update_callback' => null,
-      'schema'  => null
-    ]
-  );
-function owaf_rest_api_adresse($lieu, $adresse, $request) {
-
-    return get_post_meta($lieu['id'], 'adresse', true);
-}
-
-
-  register_rest_field(
-    'lieu',
-    'details',
-    [
-      'get_callback' => 'owaf_rest_api_details',
-      'update_callback' => null,
-      'schema' => null
-    ]
-  );
-  function owaf_rest_api_details($lieu, $details, $request) {
-
-      return get_post_meta($lieu['id'], 'details', true);
-  }
-
-  register_rest_field(
-    'lieu',
-    'commentaire',
-    [
-      'get_callback' => 'owaf_rest_api_commentaire',
-      'update_callback' => null,
-      'schema' => null
-    ]
-  );
-
-  function owaf_rest_api_commentaire($lieu, $commentaire, $request) {
-
-      return get_post_meta($lieu['id'], 'commentaire', true);
-  }
-
-  register_rest_field(
-    'dog',
-    'genre',
-    [
-      'get_callback' => 'owaf_rest_api_genre',
-      'update_callback' => null,
-      'schema' => null
-    ]
-  );
-
-
-  function owaf_rest_api_genre($dog, $genre, $request) {
-
-      return get_post_meta($dog['id'], 'genre', true);
-  }
-
-  register_rest_field(
-    'dog',
-    'naiss',
-    [
-      'get_callback' => 'owaf_rest_api_naiss',
-      'update_callback' => null,
-      'schema' => null
-    ]
-  );
-
-
-  function owaf_rest_api_naiss($dog, $naiss, $request) {
-
-      return get_post_meta($dog['id'], 'naiss', true);
-  }
-
-  register_rest_field(
-    'dog',
-    'dog_img',
-    [
-      'get_callback' => 'owaf_rest_api_dog_img',
-      'update_callback' => null,
-      'schema' => null
-    ]
-  );
-
-}
-
-function owaf_rest_api_dog_img($dog, $dog_img, $request) {
-
-    return get_post_meta($dog['id'], 'dog_img', true);
-}
-
-
-
-// rajout des 3 champs qui sont joins dans la table $user à voir si faut les rentrer dans acf
-
-add_action( 'rest_api_init', 'dog_id_add_user_data' );
-
-function dog_id_add_user_data() {
-
-register_rest_field(
-  'user',
-  'dog_id',
-    [
-        'get_callback'  => 'rest_get_user_field',
-        'update_callback'   => null,
-        'schema'            => null,
-     ]
-);
-}
-function rest_get_user_field( $user, $dog_id, $request ) {
-    return get_user_meta( $user[ 'id' ], $dog_id, true );
-}
-
-
-
-add_action( 'rest_api_init', 'adress_add_user_data' );
-
-function adress_add_user_data() {
-register_rest_field(
-  'user',
-  'ville',
-    [
-        'get_callback'  => 'rest_get_user_field',
-        'update_callback'   => null,
-        'schema'            => null,
-     ]
-);
-}
-function rest_get_user_field( $user, $ville, $request ) {
-    return get_user_meta( $user[ 'id' ], $ville, true );
-}
-
-
-add_action( 'rest_api_init', 'mail_add_user_data' );
-
-function mail_add_user_data() {
-register_rest_field(
-  'user',
-  'mail',
-    [
-        'get_callback'  => 'rest_get_user_field',
-        'update_callback'   => null,
-        'schema'            => null,
-     ]
-);
-}
-
-
-function rest_get_user_field( $user, $mail, $request ) {
-    return get_user_meta( $user[ 'id' ], $mail, true );
-}
-
-
-
-add_filter( 'rest_user_query' , 'custom_rest_user_query' );
+// Envoi de la liste des utilisateurs complete (pas uniquement ceux qui ont publiés)
 function custom_rest_user_query( $prepared_args, $request = null ) {
- unset($prepared_args['has_published_posts']);
- return $prepared_args;
+    unset($prepared_args['has_published_posts']);
+    return $prepared_args;
 }
+add_filter( 'rest_user_query' , 'custom_rest_user_query' );
